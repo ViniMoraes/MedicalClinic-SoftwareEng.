dc539384d9c2840fcc1a345d221eb7f7de13f671
ViniMoraes <moraes.vinicius@hotmail.com>
2016-10-29 18:03:59 -0200

    -

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace MedicalClinic
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);            
-            Application.Run(new Forms.InitialForm());
+            Application.Run(new Form());
         }
     }
 }
